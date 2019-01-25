e6bdc3c55609dc442458fa1e6e0e31b33256bd9d
HarryWho <calvin.bartlett42@gmail.com>
2019-01-26 10:58:34 +1100

    first commit

--- MyGrid/index.php
+++ MyGrid/index.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Site</title>
-    <link rel="stylesheet" href="css/style.css">
+    <title>Blah Blah Blah</title>
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
     <header>
         <nav>
-            <div class="brand"><a href="#">My Site</a></div>
+            <div class="brand"><h1>My Site</h1></div>
             <div class="menu">
                 <ul>
                     <li><a href="#">Home</a></li>
@@ -21,6 +21,9 @@
             </div>
         </nav>
     </header>
-    
+    <div class="side-bar">
+        
+    </div>
+
 </body>
 </html>
